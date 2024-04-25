e8e721437b5fc3d51846aea69cf450fde93724b0
sai worms <sai@wormsconnect.com>
2024-04-25 23:35:34 +0530

    Rn app

---
+++ src/pages/webView/web-view.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { WebView } from 'react-native-webview';
+
+const WebViewComponent = () => {
+    const webURL="https://pmtestingwebviewwithoutlogin.netlify.app"
+    return <View style={{flex:1}} >
+            <WebView source={{ uri: webURL }}  />
+
+    </View>
+
+
+}
+
+export default WebViewComponent
