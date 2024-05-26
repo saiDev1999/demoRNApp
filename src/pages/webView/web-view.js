7c4031c0d5e01866b65936465592223b59777527
sai worms <sai@wormsconnect.com>
2024-05-26 16:54:38 +0530

    eslint fix

--- src/pages/webView/web-view.js
+++ src/pages/webView/web-view.js
@@ -1,12 +1,11 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import {WebView} from 'react-native-webview';
 
 const WebViewComponent = () => {
   const webURL = 'https://pmtestingwebviewwithoutlogin.netlify.app';
-  return (
-    <View style={{flex: 1}}>{/* <WebView source={{uri: webURL}} /> */}</View>
-  );
+
+  return <View style={{flex: 1}}>{<WebView source={{uri: webURL}} />}</View>;
 };
 
 export default WebViewComponent;
